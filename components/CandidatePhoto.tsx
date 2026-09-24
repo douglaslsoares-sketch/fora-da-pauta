a9b182e839c15fa97b19d1356158a488d2da810f
naoeseuorg-maker <douglas.l.soares@gmail.com>
2026-09-24 10:10:45 -0300

    fix: usar dominio proprio nas fotos de candidatos

--- components/CandidatePhoto.tsx
+++ components/CandidatePhoto.tsx
@@ -54,7 +54,7 @@ export function CandidatePhoto({
     >
       {!erro ? (
         <img
-          src={`https://pub-2ff7d24c72ae4d739a71d60b1a26bb5e.r2.dev/candidatos/2026/${id}.jpg`}
+          src={`https://fotos.foradapauta.org/candidatos/2026/${id}.jpg`}
           alt={`Foto de ${name}`}
           onError={() => setErro(true)}
           style={{
